7881f1ad11a69845ce0d1c2f51b4eae51a0b5ef7
tabed10 <sabbirhossainabed@gmail.com>
2019-07-10 00:39:31 +0600

    Updated

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var http = require('http');
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello World\n");
-  });
-  server.listen(3000, 'localhost', function(){
-    console.log('Server running');
-  });
+var express = require('express');
+var app = express();
+var server= https.Server(app)
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html')
+})
+app.get('/second',function(req,res){
+    res.sendFile(__dirname+'/second.html')
+})
+// var server = http.createServer(function(req, res){
+//   res.statusCode = 200;
+//   sre.setHeader('Content-Type', 'text/html');
+//   fs.readFile('index.html', function(err,data){
+//     if (err){
+//        return console.log("File read error")
+//     }  
+    
+//     res.end(data);
+//   })
+// });
+server.listen(3000, 'localhost', function(){
+  console.log('Server running');
+});
