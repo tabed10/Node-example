1b1a6b014cd75c7db0732bdd27ac8c8b6a8ae4cb
cis101 <cis101@secs.iub.edu.bd>
2019-07-10 09:24:10 +0600

    cw 8c

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var server= https.Server(app)
+var bodyParser = require('body-parser');
 
+app.use(bodyPareser.json());
+app.use(bodyPareser.urlencoded({extended:true}));
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html')
 })
 app.get('/second',function(req,res){
     res.sendFile(__dirname+'/second.html')
+})
+app.get('/form',function(req,res){
+  res.sendFile(__dirname+'/second.html')
+})
+
+app.post('/article/new', function(){
+    console.log(req.body)
 })
 // var server = http.createServer(function(req, res){
 //   res.statusCode = 200;
